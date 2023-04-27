d16584e6df6b9810ec6610a3f1328fe32ecc4843
“Dubem <dubemobinnaesiowu@gmail.com>
2023-04-26 21:35:11 -0700

    Compiled

---
+++ a_str3.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
+ */
+void rev_string(char *s)
+{
+	int count_x = 0, i, j;
+	char *str_x, temp_x;
+
+	while (count_x >= 0)
+	{
+		if (s[count_x] == '\0')
+			break;
+		count_x++;
+	}
+	str_x = s;
+
+	for (i = 0; i < (count_x - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp_x = *(str_x + j);
+			*(str_x + j) = *(str_x + (j - 1));
+			*(str_x + (j - 1)) = temp_x;
+		}
+	}
+}
